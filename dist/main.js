7cf4f49ced8b7d94f6f7c1bc8112b1a3d25dce1e
Memo Chou <memochou1993@gmail.com>
2022-05-14 15:44:30 +0800

    Update layout

--- dist/main.js
+++ dist/main.js
@@ -1,14 +1,12 @@
 "use strict";
 class App {
     constructor() {
-        this.canvas = document.getElementById('main');
+        this.canvas = document.getElementById('canvas');
         this.ctx = this.canvas.getContext('2d');
-        if (this.ctx) {
-            this.ctx.fillStyle = "rgb(200,0,0)";
-            this.ctx.fillRect(10, 10, 55, 50);
-            this.ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-            this.ctx.fillRect(30, 30, 55, 50);
-        }
+        this.ctx.fillStyle = "rgb(200,0,0)";
+        this.ctx.fillRect(10, 10, 55, 50);
+        this.ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+        this.ctx.fillRect(30, 30, 55, 50);
     }
 }
 new App();
