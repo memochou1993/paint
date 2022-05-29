56724be19ccf680ba47a25f01e81c1ad7797024b
Memo Chou <memochou1993@gmail.com>
2022-05-29 16:52:05 +0800

    Update styles

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
-export const ANCHOR_FILL_STYLE = 'darkcyan';
+export const ANCHOR_FILL_STYLE = 'steelblue';
 export const ANCHOR_HEIGHT = 10;
 export const ANCHOR_WIDTH = 10;
-export const SHAPE_FILL_STYLE = 'azure';
-export const SHAPE_STROKE_STYLE = 'cadetblue';
+export const SHAPE_FILL_STYLE = 'aliceblue';
+export const SHAPE_STROKE_STYLE = 'steelblue';
