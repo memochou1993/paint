e802812b31c82ad7d1ac943b227274543f240768
Memo Chou <memochou1993@gmail.com>
2022-06-02 15:54:28 +0800

    Optimize ellipse widget

--- src/widgets/EllipseWidget.ts
+++ src/widgets/EllipseWidget.ts
@@ -6,21 +6,29 @@ export default class EllipseWidget extends Widget {
 
   private isDrawing = false;
 
+  private startX = 0;
+
+  private startY = 0;
+
   click(): void {}
 
   mouseDown(e: MouseEvent): void {
     this.isDrawing = true;
     this.shape = new Ellipse(this.ctx);
-    this.shape.setX(e.offsetX);
-    this.shape.setY(e.offsetY);
+    this.startX = e.offsetX;
+    this.startY = e.offsetY;
   }
 
   mouseMove(e: MouseEvent): void {
     if (!this.isDrawing) return;
     this.clear();
     this.redraw();
-    this.shape.setRadiusX(Math.abs(e.offsetX - this.shape.x) * Math.sqrt(2));
-    this.shape.setRadiusY(Math.abs(e.offsetY - this.shape.y) * Math.sqrt(2));
+    const offsetX = (e.offsetX - this.startX) / 2;
+    const offsetY = (e.offsetY - this.startY) / 2;
+    this.shape.setX(this.startX + offsetX);
+    this.shape.setY(this.startY + offsetY);
+    this.shape.setRadiusX(Math.abs(offsetX) * Math.sqrt(2));
+    this.shape.setRadiusY(Math.abs(offsetY) * Math.sqrt(2));
     this.shape.draw();
   }
 
