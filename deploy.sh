fb121efa897fdbcdc417d90f2bd4fc715c41fb40
Memo Chou <memochou1993@gmail.com>
2022-06-16 12:37:22 +0800

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:memochou1993/paint.git main:gh-pages
+
+cd -
